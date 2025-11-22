9a426d7b27bbd6b00c656280e52bc527dce9ecb1
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2025-11-23 02:32:06 +0530

    started javascript

---
+++ javascript/JS basics 1/index.js
@@ -0,0 +1,14 @@
+// line below is used to print on the console window
+console.log('Hello World bhai log');
+let a = 'hari';
+let b = 4;
+console.log(a);
+console.log(b);
+
+//const num=12;
+//num=13; // you cannot change the value of a constant variable
+
+let lastName='harivansh';
+console.log(lastName);
+lastName=3;
+console.log(lastName);

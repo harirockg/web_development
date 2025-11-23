59804b6e155b787ca190ea51031facd3108edf02
Harivansh Chauhan <144122932+harirockg@users.noreply.github.com>
2025-11-23 15:33:42 +0530

    js basic 1

--- javascript/JS basics 1/index.js
+++ javascript/JS basics 1/index.js
@@ -1,14 +1,81 @@
+//js basics 1
+
 // line below is used to print on the console window
-console.log('Hello World bhai log');
-let a = 'hari';
-let b = 4;
-console.log(a);
-console.log(b);
+// console.log('Hello World bhai log');
+// let a = 'hari';
+// let b = 4;
+// console.log(a);
+// console.log(b);
 
-//const num=12;
-//num=13; // you cannot change the value of a constant variable
+// //const num=12;
+// //num=13; // you cannot change the value of a constant variable
 
-let lastName='harivansh';
-console.log(lastName);
-lastName=3;
-console.log(lastName);
+// let lastName='harivansh';
+// console.log(lastName);
+// lastName=3;
+// console.log(lastName);
+
+// let age=17;
+// let status=(age >= 18) ? "yes vote" : "no vote";
+// console.log(status);
+
+
+//control statements
+
+// let marks=55;
+// if (marks >= 90){
+//     console.log('A');
+// }
+// else if (marks >= 80){
+//     console.log('B');
+// }else if (marks >= 70){
+//     console.log('C');
+// }else if (marks >= 60){
+//     console.log('D');
+// }else{
+//     console.log('E');
+// }
+
+
+//switch case
+
+// let num=2;
+// switch(num){
+//     case 1:console.log('A');
+//     break;
+//     case 2:console.log('B');
+//     break;
+//     case 3:console.log('C');
+//     break;  
+//     default:console.log('D');
+// }
+
+
+//loops
+
+// for (let i=0;i<5;i++){
+//     console.log(i);
+//     console.log('harivansh');
+// }
+
+
+//while loop
+
+// let j=0;
+// while (j<10){
+//     console.log(j);
+//     j++;
+// }
+
+
+//do while loop
+
+// let k=0;
+// do {
+//     console.log(k);
+//     k++;
+// }while (k<5);
+
+
+
+
